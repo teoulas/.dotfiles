437f9149a8ff477589678675821c0d838d3a0b00
Theodoros Orfanidis <teoulas@gmail.com>
2021-04-06 09:12:02 +0200

    Add wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require "wezterm";
+return {
+  color_scheme = "Builtin Tango Dark",
+  bold_brightens_ansi_colors = false,
+  enable_scroll_bar = true,
+  enable_wayland = true,
+  font = wezterm.font("Fantasque Sans Mono"),
+  font_size = 13,
+  font_antialias = "Subpixel",
+  font_hinting = "Vertical",
+}
